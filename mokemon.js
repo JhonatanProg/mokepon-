86d93a746f89971ca70f8b2a1892344763ebe682
JhonatanProg <kleverlalangui@gmail.com>
2024-08-23 09:30:28 -0500

    inicia mokepon

---
+++ mokemon.js
@@ -0,0 +1,5 @@
+function selecionarMascotaJugador(){
+    alert("selecioanste tu mascota")
+}
+let botonMascotaJugador = document.getElementById("boton-mascota")
+botonMascotaJugador.addEventListener("click", selecionarMascotaJugador)
