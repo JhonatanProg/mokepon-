07dc8698ffb704aaaff730453ce124bb6ff7f3ec
JhonatanProg <kleverlalangui@gmail.com>
2024-08-24 15:56:29 -0500

    verificando mascota selecinada

--- mokemon.js
+++ mokemon.js
@@ -1,5 +1,24 @@
-function selecionarMascotaJugador(){
-    alert("selecioanste tu mascota")
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", selecionarMascotaJugador)
+
 }
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", selecionarMascotaJugador)
+function selecionarMascotaJugador(){
+    let inputwizar = document.getElementById("wizar");
+    let inputjinx = document.getElementById("jix");
+    let inputluxor = document.getElementById("luxor");
+
+
+
+    if (inputwizar.checked) {
+        alert("selecionaste Wizar")   
+    } else if(inputjinx.checked) {
+        alert("selecionaste jinx")   
+    }else if(inputluxor.checked) {
+        alert("selecionaste luxor")   
+    }else{
+        alert("no selecioanste ninguna mascota")
+    }
+}
+
+window.addEventListener("load", iniciarJuego)
