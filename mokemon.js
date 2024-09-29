cac523478559fa0dd9e1a6b710553077f2650c5f
JhonatanProg <kleverlalangui@gmail.com>
2024-09-28 23:13:41 -0500

    añadiendo clases y objetos clase 48

--- mokemon.js
+++ mokemon.js
@@ -28,6 +28,20 @@ let ataqueEnemigo;
 let vidasJugador = 3;
 let vidasEnemigo = 3;
 
+class Mokepon{
+    constructor(nombre, foto, vida) {
+        this.nombre = nombre
+        this.foto = foto
+        this.vida = vida
+    }
+}
+
+let wizar = new Mokepon('wizar', './imagenes pokemones/mokepons_mokepon_capipepo_attack.webp', 5)
+
+let jinx = new Mokepon('jinx', './imagenes pokemones/mokepons_mokepon_hipodoge_attack.webp', 5) 
+
+let luxor = new Mokepon('luxor', './imagenes pokemones/mokepons_mokepon_ratigueya_attack.webp', 5)
+
 //funcion para iniciar el juego al mismo 
 function iniciarJuego(){
     
